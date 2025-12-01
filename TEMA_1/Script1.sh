b6a9da8cb0a22bca3a1eea284ce7ed63d62006b8
Hugopdam <hugopdam@gmail.com>
2025-12-01 09:35:58 +0100

    Update comment for Apache ports configuration file
    Clarify the location of the Apache configuration file.

--- TEMA_1/Script1.sh
+++ TEMA_1/Script1.sh
@@ -3,7 +3,7 @@
 #Crea un script que añada un puerto de escucha en el fichero de configuración de Apache. 
 #El puerto se recibirá como parámetro en la llamada y se comprobará que no esté ya presente en el fichero de configuración.
 
-#/etc/apache2/ports.conf
+#este fichero se pondria en /etc/apache2/ports.conf
 
 if [ $# -ne 1 ]; then
     echo "Introduce solo un puerto"
